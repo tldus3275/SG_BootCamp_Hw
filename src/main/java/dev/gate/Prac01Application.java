b631e40b0756295788d214f584a560b8767f12d0
SeHoon Kim <mentalveritas@gmail.com>
2017-01-17 17:22:02 +0900

    First Commit

---
+++ src/main/java/dev/gate/Prac01Application.java
@@ -0,0 +1,12 @@
+package dev.gate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prac01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prac01Application.class, args);
+	}
+}
